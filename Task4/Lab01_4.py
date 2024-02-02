2a7f4c869c94e5ad590e047a76a0ba77f9bb2142
AHSAN HABIB <143648731+ahsanauddry027@users.noreply.github.com>
2024-02-02 10:33:38 +0600

    Update Lab01_4.py

--- Task4/Lab01_4.py
+++ Task4/Lab01_4.py
@@ -1,6 +1,5 @@
 readFile = open('input4.txt','r')
 writeFile = open('output4.txt','w')
-
 intputnum = readFile.readline().split("\n")[0]
 sort = {}
 for nums in range(int(intputnum)):
@@ -9,7 +8,6 @@ for nums in range(int(intputnum)):
     place = info[4]
     time1 = info[6].split("\n")[0].split(":")[0]
     time2 = info[6].split("\n")[0]
-    
     names = info[0]
     num=0
     for nums in names:
@@ -19,32 +17,28 @@ for nums in range(int(intputnum)):
     else:
         sort[ord(names[0])]+= [[names,num,int(time1),place,time2]]
 
-def ascending(dic):
+def lexicographical(dic):
     newlist = list(dic.items())
     for i in range(len(newlist)):
         j=i
         while j>0 and newlist[j-1][0]>newlist[j][0]:
             newlist[j],newlist[j-1]=newlist[j-1],newlist[j]
             j-=1
-    return newlist
     
-newsort= dict(ascending(sort))
-
-for nums in newsort:
-    key = newsort[nums]
-
-    for i in range(len(key)):
-        k=i
-        while k>0:
-            if key[k-1][1]>key[k][1]:
-                key[k],key[k-1]=key[k-1],key[k]
-            elif key[k-1][1]==key[k][1]:
-                if key[k-1][2]<key[k][2]:
-                  key[k],key[k-1]=key[k-1],key[k]
-            k-=1
+    for nums in dic:
+        key = dic[nums]
+        for i in range(len(key)):
+            k=i
+            while k>0:
+                if key[k-1][1]>key[k][1]:
+                    key[k],key[k-1]=key[k-1],key[k]
+                elif key[k-1][1]==key[k][1]:
+                    if key[k-1][2]<key[k][2]:
+                        key[k],key[k-1]=key[k-1],key[k]
+                k-=1
+                
+        for num in key:
+            stri = f"{num[0]} will departure for {num[3]} at {num[4]}\n"
+            writeFile.write(stri)
             
-for nums in newsort:
-    
-    for num in newsort[nums]:
-        stri = f"{num[0]} will departure for {num[3]} at {num[4]}\n"
-        writeFile.write(stri)
+lexicographical(sort)
