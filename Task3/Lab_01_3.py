a69c9dfc13c226bf086ef5e66a000a008c6b2523
AHSAN HABIB <143648731+ahsanauddry027@users.noreply.github.com>
2024-02-02 16:05:27 +0600

    Update Lab_01_3.py

--- Task3/Lab_01_3.py
+++ Task3/Lab_01_3.py
@@ -1,30 +1,42 @@
-readFile = open('input3.txt','r')
-writeFile = open('output3.txt','w')
-noStudents = int(readFile.readline().strip("\n"))
-studentId={}
+inputs = [['input3a.txt','output3a.txt'],['input3b.txt','output3b.txt']]
 
-ids = readFile.readline().split(" ")
-marks = readFile.readline().split(" ")
+for num in inputs:
+    readFile = open(num[0],'r')
+    writeFile = open(num[1],'w')
 
-for nums in range(noStudents):
-    studentId[int(ids[nums])] = int(marks[nums])
-    
-def sorting(dict_items):
-    items = list(dict_items.items())
-    for i in range(len(items)): 
-        j=i
-        while j>0:
-            if items[j-1][1]<items[j][1]:
-                items[j],items[j-1]=items[j-1],items[j]
-            elif items[j-1][1]==items[j][1]:
-                if items[j-1][0]>items[j][0]:
-                    items[j],items[j-1]=items[j-1],items[j]
-            j-=1
-             
-    return items
+    noStudents = int(readFile.readline().strip("\n"))
+    studentId={}
 
-sorted = dict(sorting(studentId))
-for nums in sorted:
-    str=f"ID: {nums} Mark: {sorted[nums]}"
-    writeFile.write(str+"\n")
-writeFile.close()
+    ids = readFile.readline().split(" ")
+    marks = readFile.readline().split(" ")
+
+    for nums in range(noStudents):
+        studentId[int(ids[nums])] = int(marks[nums])
+        
+    def Selectionsorting(dict_items):
+        items = list(dict_items.items())
+        for i in range(len(items)): 
+            currMin = items[i]
+            j = i
+            num = 0
+            flag= False
+            while j<len(items):
+                if  currMin[1]<items[j][1]:
+                    currMin = items[j]
+                    num = j
+                    flag=True
+                elif currMin[1]==items[j][1]:
+                    if currMin[0]>items[j][0]:
+                        currMin = items[j]
+                        num = j
+                        flag=True
+                j+=1
+            if flag==True:
+                items[i],items[num] = currMin,items[i] 
+        return items
+    sort = dict(Selectionsorting(studentId))
+    for nums in sort:
+        str=f"ID: {nums} Mark: {sort[nums]}"
+        writeFile.write(str+"\n")
+    readFile.close()
+    writeFile.close()
