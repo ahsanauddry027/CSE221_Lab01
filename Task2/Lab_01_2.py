5467114c738fc35964a88187da1dec138d0c5a90
AHSAN HABIB <143648731+ahsanauddry027@users.noreply.github.com>
2024-02-02 23:17:51 +0600

    Update Lab_01_2.py

--- Task2/Lab_01_2.py
+++ Task2/Lab_01_2.py
@@ -1,30 +1,31 @@
-readFile = open('input2.txt','r')
-writeFile = open('output2.txt','w')
-length = int(readFile.readline().split("\n")[0])
-def bubble_sort(arr,n):
-    
-    for i in range(n):
-        flag= False
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                flag = True
-        if flag ==False:
-            break
-        
-        
+inputs = [['input2a.txt','output2a.txt'],['input2b.txt','output2b.txt']]
+for num in inputs:
+   
+    readFile = open(num[0],'r')
+    writeFile = open(num[1],'w')
+    length = int(readFile.readline().split("\n")[0])
 
-# Example usage:
-arr = []
-list1= readFile.readline().split(" ")
-for i in list1:
-    arr.append(int(i))
-bubble_sort(arr,length)
-for n in range(len(arr)):
-    # print(n)
-    if n==len(arr)-1:
-        writeFile.write(str(arr[n]))
-    else:
-        writeFile.write(str(arr[n])+" ")
-        
-print("Sorted array:", arr)
+    def bubble_sort(arr,n):
+        for i in range(n):
+            flag= False
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    flag = True
+            if flag ==False:
+                break
+            
+    # Example usage:
+    arr = []
+    list1= readFile.readline().split(" ")
+    for i in list1:
+        arr.append(int(i))
+    bubble_sort(arr,length)
+    for n in range(len(arr)):
+        if n==len(arr)-1:
+            writeFile.write(str(arr[n]))
+        else:
+            writeFile.write(str(arr[n])+" ")
+            
+    readFile.close()
+    writeFile.close()
